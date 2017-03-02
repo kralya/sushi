ed76081c28e20a9fbe0d4ec4023b9ecec3299cf0
parap <spalchinda@gmail.com>
2017-03-02 23:17:25 +0200

    Add sample base.html.twig and some temporary files to keep playing with it

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -40,4 +40,26 @@ class DefaultController extends BaseController
         
         return $this->render('default/test.html.twig', $params);
     }
+    
+    /**
+     * @Route("/sample", name="sample")
+     */
+    public function sampleAction(Request $request)
+    {
+        
+        $params = [];
+        $params['description'] = 'Доставка суши в Запорожье. Самый изысканный выбор. Всегда только свежие продукты и отменное качество блюд. Бесплатная доставка от 250 000 рублей. Закажите прямо сейчас';
+        $params['keywords'] = 'доставка суши в запорожье, суши на дом, бесплатная доставка, суши круглосуточно доставка, доставка еды, службы доставки еды, лучшие суши в минске, заказ суши на дом, суши, нигири, маки, ура маки, роллы, лапша, сашими, салаты, закуски, рис, десерты, васаби';
+        $params['title'] = 'Доставка суши в Минске. Sushi House Ресторан Японской кухни (Суши Хаус)';
+        $params['info_seller1'] = 'OOO "Глобалпрогрупп" г.Минск, ул. Казинца, 123, оф.6 УНП 192610415';
+        $params['info_seller2'] = 'Интернет-магазин www.sushihouse.by зарегистрирован в Торговом реестре Республики Беларусь №331264 от 18.05.2016';
+        $params['phone'] = '668-11-12';
+        
+        $params['vk'] = 'https://vk.com/sushihouseby';
+        $params['fb'] = 'https://www.facebook.com/sushihouseby';
+        $params['ig'] = 'https://instagram.com/sushihouseby';
+        $params['ok'] = 'http://ok.ru/group/51994461470904';
+        
+        return $this->render('default/my.html.twig', $params);
+    }
 }
