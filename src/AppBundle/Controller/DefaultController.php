1fd94ac1000370c3dc1e1e2b0b83bc894d04c913
parap <spalchinda@gmail.com>
2017-03-02 18:37:59 +0200

    Add sample menu page processing folder names properly

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,13 +8,34 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends BaseController
 {
     /**
-     * @Route("/hello", name="homepage")
+     * @Route("/", name="homepage")
+     * @Route("/hello")
      */
     public function indexAction(Request $request)
     {
         $category = $this->getRepo('Category')->findOneByUrl('sushi');
         $products = $this->getRepo('Product')->findByCategory($category);
         
+        $params = ['products' => $products];
+        
+        return $this->render('default/test.html.twig', $params);
+    }
+
+    /**
+     * @Route("/menu", name="menu")
+     * @Route("/menu/{category}")
+     */
+    public function menuAction(Request $request)
+    {
+        $url = $request->get('category');
+        $category = $this->getRepo('Category')->findOneByUrl($url);
+        
+        if( $url && !$category) {
+            throw $this->createNotFoundException('The product does not exist');
+        }
+        
+        $repo = $this->getRepo('Product');
+        $products = $url ? $repo->findByCategory($category) : $repo->findAll();
         $params = ['products' => $products];
         
         return $this->render('default/test.html.twig', $params);
