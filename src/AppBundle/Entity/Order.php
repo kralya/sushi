2390ae2d477c9be65ce3385864f88f85b016e81e
parap <spalchinda@gmail.com>
2017-03-02 16:38:45 +0200

    Initial commit

---
+++ src/AppBundle/Entity/Order.php
@@ -0,0 +1,376 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Order
+ *
+ * @ORM\Table(name="order")
+ * @ORM\Entity
+ */
+class Order
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="string", length=1024, nullable=false)
+     */
+    private $address;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="delivery_time", type="datetime", nullable=false)
+     */
+    private $deliveryTime;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="total", type="integer", nullable=false)
+     */
+    private $total;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="first_name", type="string", length=256, nullable=false)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="last_name", type="string", length=256, nullable=false)
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phone", type="string", length=256, nullable=false)
+     */
+    private $phone;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="delivery_type", type="boolean", nullable=false)
+     */
+    private $deliveryType;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="payment_type", type="boolean", nullable=false)
+     */
+    private $paymentType;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="short_change", type="integer", nullable=false)
+     */
+    private $shortChange;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="note", type="string", length=16000, nullable=false)
+     */
+    private $note;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="status", type="boolean", nullable=false)
+     */
+    private $status;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     *
+     * @return Order
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set deliveryTime
+     *
+     * @param \DateTime $deliveryTime
+     *
+     * @return Order
+     */
+    public function setDeliveryTime($deliveryTime)
+    {
+        $this->deliveryTime = $deliveryTime;
+
+        return $this;
+    }
+
+    /**
+     * Get deliveryTime
+     *
+     * @return \DateTime
+     */
+    public function getDeliveryTime()
+    {
+        return $this->deliveryTime;
+    }
+
+    /**
+     * Set total
+     *
+     * @param integer $total
+     *
+     * @return Order
+     */
+    public function setTotal($total)
+    {
+        $this->total = $total;
+
+        return $this;
+    }
+
+    /**
+     * Get total
+     *
+     * @return integer
+     */
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return Order
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return Order
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set phone
+     *
+     * @param string $phone
+     *
+     * @return Order
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set deliveryType
+     *
+     * @param boolean $deliveryType
+     *
+     * @return Order
+     */
+    public function setDeliveryType($deliveryType)
+    {
+        $this->deliveryType = $deliveryType;
+
+        return $this;
+    }
+
+    /**
+     * Get deliveryType
+     *
+     * @return boolean
+     */
+    public function getDeliveryType()
+    {
+        return $this->deliveryType;
+    }
+
+    /**
+     * Set paymentType
+     *
+     * @param boolean $paymentType
+     *
+     * @return Order
+     */
+    public function setPaymentType($paymentType)
+    {
+        $this->paymentType = $paymentType;
+
+        return $this;
+    }
+
+    /**
+     * Get paymentType
+     *
+     * @return boolean
+     */
+    public function getPaymentType()
+    {
+        return $this->paymentType;
+    }
+
+    /**
+     * Set shortChange
+     *
+     * @param integer $shortChange
+     *
+     * @return Order
+     */
+    public function setShortChange($shortChange)
+    {
+        $this->shortChange = $shortChange;
+
+        return $this;
+    }
+
+    /**
+     * Get shortChange
+     *
+     * @return integer
+     */
+    public function getShortChange()
+    {
+        return $this->shortChange;
+    }
+
+    /**
+     * Set note
+     *
+     * @param string $note
+     *
+     * @return Order
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    /**
+     * Get note
+     *
+     * @return string
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+
+    /**
+     * Set status
+     *
+     * @param boolean $status
+     *
+     * @return Order
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return boolean
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
---
+++ src/AppBundle/Entity/Product.php
@@ -0,0 +1,345 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Product
+ *
+ * @ORM\Table(name="product")
+ * @ORM\Entity
+ */
+class Product
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=256, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contains", type="string", length=1024, nullable=false)
+     */
+    private $contains;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="price", type="integer", nullable=false)
+     */
+    private $price;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="price_old", type="integer", nullable=false)
+     */
+    private $priceOld;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="weight", type="string", length=256, nullable=false)
+     */
+    private $weight;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="string", length=1024, nullable=false)
+     */
+    private $image;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="sticker_top", type="smallint", nullable=false)
+     */
+    private $stickerTop;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="sticker_bottom", type="smallint", nullable=false)
+     */
+    private $stickerBottom;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="recommended", type="integer", nullable=false)
+     */
+    private $recommended;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="category_id", type="integer", nullable=false)
+     */
+    private $categoryId;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Product
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set contains
+     *
+     * @param string $contains
+     *
+     * @return Product
+     */
+    public function setContains($contains)
+    {
+        $this->contains = $contains;
+
+        return $this;
+    }
+
+    /**
+     * Get contains
+     *
+     * @return string
+     */
+    public function getContains()
+    {
+        return $this->contains;
+    }
+
+    /**
+     * Set price
+     *
+     * @param integer $price
+     *
+     * @return Product
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return integer
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set priceOld
+     *
+     * @param integer $priceOld
+     *
+     * @return Product
+     */
+    public function setPriceOld($priceOld)
+    {
+        $this->priceOld = $priceOld;
+
+        return $this;
+    }
+
+    /**
+     * Get priceOld
+     *
+     * @return integer
+     */
+    public function getPriceOld()
+    {
+        return $this->priceOld;
+    }
+
+    /**
+     * Set weight
+     *
+     * @param string $weight
+     *
+     * @return Product
+     */
+    public function setWeight($weight)
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+
+    /**
+     * Get weight
+     *
+     * @return string
+     */
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     *
+     * @return Product
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set stickerTop
+     *
+     * @param integer $stickerTop
+     *
+     * @return Product
+     */
+    public function setStickerTop($stickerTop)
+    {
+        $this->stickerTop = $stickerTop;
+
+        return $this;
+    }
+
+    /**
+     * Get stickerTop
+     *
+     * @return integer
+     */
+    public function getStickerTop()
+    {
+        return $this->stickerTop;
+    }
+
+    /**
+     * Set stickerBottom
+     *
+     * @param integer $stickerBottom
+     *
+     * @return Product
+     */
+    public function setStickerBottom($stickerBottom)
+    {
+        $this->stickerBottom = $stickerBottom;
+
+        return $this;
+    }
+
+    /**
+     * Get stickerBottom
+     *
+     * @return integer
+     */
+    public function getStickerBottom()
+    {
+        return $this->stickerBottom;
+    }
+
+    /**
+     * Set recommended
+     *
+     * @param integer $recommended
+     *
+     * @return Product
+     */
+    public function setRecommended($recommended)
+    {
+        $this->recommended = $recommended;
+
+        return $this;
+    }
+
+    /**
+     * Get recommended
+     *
+     * @return integer
+     */
+    public function getRecommended()
+    {
+        return $this->recommended;
+    }
+
+    /**
+     * Set categoryId
+     *
+     * @param integer $categoryId
+     *
+     * @return Product
+     */
+    public function setCategoryId($categoryId)
+    {
+        $this->categoryId = $categoryId;
+
+        return $this;
+    }
+
+    /**
+     * Get categoryId
+     *
+     * @return integer
+     */
+    public function getCategoryId()
+    {
+        return $this->categoryId;
+    }
+}
