779632e1f82be93255ee9203f20951419556222f
parap <spalchinda@gmail.com>
2017-03-03 12:27:23 +0200

    Extract calbitrix JS

---
+++ web/js/callbitrix.js
@@ -0,0 +1,11 @@
+var _ba = _ba || [];
+_ba.push(["aid", "afd580abdac75e38b9e1b502ea828a2a"]);
+_ba.push(["host", "sushihouse.by"]);
+(function () {
+    var ba = document.createElement("script");
+    ba.type = "text/javascript";
+    ba.async = true;
+    ba.src = (document.location.protocol == "https:" ? "https://" : "http://") + "bitrix.info/ba.js";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(ba, s);
+})();
