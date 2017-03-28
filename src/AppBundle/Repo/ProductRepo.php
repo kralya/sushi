8c493dcaebbacdf0802bbd01170c5e2ca138a4c5
parap <spalchinda@gmail.com>
2017-03-28 15:43:35 +0300

    Add findPopular() m-d

--- src/AppBundle/Repo/ProductRepo.php
+++ src/AppBundle/Repo/ProductRepo.php
@@ -6,4 +6,15 @@ use Doctrine\ORM\EntityRepository;
 
 class ProductRepo extends EntityRepository
 {
+    public function findPopular($min)
+    {
+        $q = $this->createQueryBuilder('p')
+                ->select()
+                ->where('p.recommended IS NOT NULL')
+                ->andWhere('p.recommended > :minn')
+                ->setParameter('minn', $min)
+                ;
+        
+        return $q->getQuery()->execute();
+    }
 }
